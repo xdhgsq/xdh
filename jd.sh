b80e7ded2c4fc10d7b49376e12caf3524d71b222
xdhgsq <94951545+xdhgsq@users.noreply.github.com>
2022-06-27 07:32:51 +0800

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -549,7 +549,7 @@ EOF
 		export first="false"
 		export launchid="1177da281193920b5ccba3c4ef3bd767"
 	fi
-	#$node  $openwrt_script/JD_Script/js/jd_jxmfl.js			#京喜免费领（请自行替换为自己的变量）
+	$node  $openwrt_script/JD_Script/js/jd_jxmfl.js			#京喜免费领（请自行替换为自己的变量）
 	$node $openwrt_script/JD_Script/js/jd_fruit.js & #东东水果，6-9点 11-14点 17-21点可以领水滴
 	$node $openwrt_script/JD_Script/js/jd_jxlhb.js & #惊喜领红包
 }
