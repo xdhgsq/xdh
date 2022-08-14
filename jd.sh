748f2ad6464976f3a4c5375458964a441446279d
xdhgsq <94951545+xdhgsq@users.noreply.github.com>
2022-08-14 11:33:13 +0800

    jd_tanwei.js			探味奇遇记

--- jd.sh
+++ jd.sh
@@ -318,6 +318,7 @@ cat >$dir_file/config/tmp/KingRan_url.txt <<EOF
 	jd_qqxing.js			#星系牧场
 	jd_jxmfl.js			#京喜免费领
 	jd_supermh.js			#京东超级盲盒
+	jd_tanwei.js			探味奇遇记
 EOF
 
 for script_name in `cat $dir_file/config/tmp/KingRan_url.txt | grep -v "#.*js" | awk '{print $1}'`
@@ -533,6 +534,7 @@ cat >/tmp/jd_tmp/ccr_run <<EOF
 	jd_joypark_task.js		#汪汪乐园每日任务,只做部分任务
 	jd_qqxing.js			#星系牧场
 	jd_supermh.js			#京东超级盲盒
+	jd_tanwei.js			探味奇遇记
 EOF
 	for i in `cat /tmp/jd_tmp/ccr_run | grep -v "#.*js" | awk '{print $1}'`
 	do
